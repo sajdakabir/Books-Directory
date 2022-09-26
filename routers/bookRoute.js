a4edad5c8dd98d5725e8b2e16577e3c639b9f63d
sajdakabir <sajda.kbir.com>
2022-09-26 10:52:12 +0530

    created all the routers

--- routers/bookRoute.js
+++ routers/bookRoute.js
@@ -1,2 +1,17 @@
 const express=require('express');
-// const bookRouter=express.
+const bookRouter=express.Router();
+
+
+bookRouter
+.route('/addBook')
+.post(addBook)
+
+bookRouter
+.route('/:id')
+.get(getBook)
+.patch(updateBookInfo)
+.delete(deleteBookInfo)
+
+bookRouter
+.route('/')
+.get(getAllBooks)
